b5f4a85a0af1cd371e0536e2418f0dd04f0b719d
ManavVaishnani0 <manav29t@gmail.com>
2026-03-19 10:25:33 +0530

    few conversions

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33a"
+let score = undefined
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -7,4 +7,22 @@ let valueInNumber = Number(score);
 
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+// "33" => 33
+// "33a" => NaN
+// "true" => 1 || "false" => 0
+
+let isLoggedIn = "manav"
+let booleanIsLoogedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+// 1 => true || 0 => false
+// "" => false
+//"manav" => true
+
+let someNumber =33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
 
