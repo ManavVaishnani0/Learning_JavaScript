ee3d6d5d8ac7a2b7c903c2cf9f09c19dce9f7867
ManavVaishnani0 <manav29t@gmail.com>
2026-03-19 09:57:46 +0530

    Added new file in subfolder

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are use code editor not browser
+
+console.log(3+3); console.log("Manav") // not readable
+
+console.log("Hello")
+
+console.log(3
+    +3
+); // not use these type of way
+
+
