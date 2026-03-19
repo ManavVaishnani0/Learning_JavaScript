59202ca7e83c042a949fa72d2a5cf73ff6c1ab70
ManavVaishnani0 <manav29t@gmail.com>
2026-03-19 15:12:59 +0530

    added few comparision

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -6,8 +6,19 @@ console.log(2 == 1);
 console.log(2 != 1);
 
 //comparison in dataTypes
-
 console.log("2" > 1);
 console.log("02" < 1);
 console.log("02" > 1);
 
+//when == operator are there null work as null but in case of comparison null works as 0
+console.log(null > 0); //false
+console.log(null >= 0); //true
+console.log(null == 0); //false
+
+//undefined give false in whole three cases
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// ===
+console.log("2" === 2); // false
